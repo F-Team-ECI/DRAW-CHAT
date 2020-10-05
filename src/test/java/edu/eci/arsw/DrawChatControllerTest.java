d247ab265635f4238bbd6375dfe56cd79f0f814f
Germán Ospina <german.ospina-q@mail.escuelaing.edu.co>
2020-10-04 23:32:37 -0500

    DirtiesContextBeforeClass

--- src/test/java/edu/eci/arsw/DrawChatControllerTest.java
+++ src/test/java/edu/eci/arsw/DrawChatControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,6 +19,7 @@ import edu.eci.arsw.application.services.DrawChatService;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = DrawChatApp.class)
 @ActiveProfiles("test")
+@DirtiesContext(classMode = ClassMode.BEFORE_CLASS) 
 @AutoConfigureMockMvc
 public class DrawChatControllerTest {
 
