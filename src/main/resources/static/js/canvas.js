8c69acf31d42198a0c29bd1b5681d6668d5ffa7b
alejovasquero <alejo_704@hotmail.com>
2020-10-13 14:11:30 -0500

    Making Canvas

---
+++ src/main/resources/static/js/canvas.js
@@ -0,0 +1,19 @@
+var canvas = (function () {
+
+
+
+    return {
+
+        init: function () {
+            var canvas = document.getElementById("myCanvas");
+            //canvas.width = window.innerWidth;
+            //canvas.height = window.innerHeight;
+            console.log("UYY");
+        }
+
+
+    }
+
+
+
+})();
