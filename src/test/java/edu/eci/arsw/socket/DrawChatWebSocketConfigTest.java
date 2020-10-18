c7b45f51c2bc6776ea8023de6b998263e180fd26
Germán Ospina <german.ospina-q@mail.escuelaing.edu.co>
2020-10-18 13:26:54 -0500

    Delete Configuration

--- src/test/java/edu/eci/arsw/socket/DrawChatWebSocketConfigTest.java
+++ src/test/java/edu/eci/arsw/socket/DrawChatWebSocketConfigTest.java
@@ -1,46 +1,14 @@
 package edu.eci.arsw.socket;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.TimeoutException;
-
-import org.junit.Before;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.MethodSorters;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.messaging.simp.stomp.StompFrameHandler;
-import org.springframework.messaging.simp.stomp.StompHeaders;
-import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.annotation.DirtiesContext.ClassMode;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
-import org.springframework.web.socket.sockjs.client.SockJsClient;
-import org.springframework.web.socket.sockjs.client.WebSocketTransport;
-import static java.util.Arrays.asList;
-import static java.util.concurrent.TimeUnit.SECONDS;
-
-import edu.eci.arsw.application.DrawChatApp;
-import junit.framework.Assert;
-
-import java.lang.reflect.Type;
-
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = DrawChatApp.class, webEnvironment = WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
-@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 
 public class DrawChatWebSocketConfigTest {
+	
+	@Test
+	public void socketTest() {
+		
+	}
+	
 	/**
 	@LocalServerPort
 	private Integer port;
